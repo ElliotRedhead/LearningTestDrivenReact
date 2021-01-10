f0ab07487eb125d643b6f4b002935ab370305233
ElliotRedhead <elliot.redhead@gmail.com>
2021-01-10 20:17:21 +0000

    Refactor tests.

--- simple-app/src/App.test.js
+++ simple-app/src/App.test.js
@@ -5,32 +5,35 @@ import App from "./App";
 
 Enzyme.configure({ adapter: new EnzymeAdapter() });
 
+const setup = () => shallow(<App />);
+const findByTestAttr = (wrapper, value) => wrapper.find(`[data-test="${value}"]`);
+
 test("renders without error", () => {
-	const wrapper = shallow(<App />);
-	const appComponent = wrapper.find("[data-test='component-app']");
+	const wrapper = setup();
+	const appComponent = findByTestAttr(wrapper, "component-app");
 	expect(appComponent.length).toBe(1);
 });
 
 test("renders button", () => {
-	const wrapper = shallow(<App />);
-	const button = wrapper.find("[data-test='increment-button']");
+	const wrapper = setup();
+	const button = findByTestAttr(wrapper, "increment-button");
 	expect(button.length).toBe(1);
 });
 
 test("renders counter display", () => {
-	const wrapper = shallow(<App />);
-	const counterDisplay = wrapper.find("[data-test='counter-display']");
+	const wrapper = setup();
+	const counterDisplay = findByTestAttr(wrapper, "counter-display");
 	expect(counterDisplay.length).toBe(1);
 });
 
 test("counter starts at 0", () => {
-	const wrapper = shallow(<App />);
-	const appComponent = wrapper.find("[data-test='component-app']");
+	const wrapper = setup();
+	const appComponent = findByTestAttr(wrapper, "component-app");
 	expect(appComponent.length).toBe(1);
 });
 
 test("clicking on button increments counter display", () => {
-	const wrapper = shallow(<App />);
-	const appComponent = wrapper.find("[data-test='component-app']");
+	const wrapper = setup();
+	const appComponent = findByTestAttr(wrapper, "component-app");
 	expect(appComponent.length).toBe(1);
 });

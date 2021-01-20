f468d8408e2e7719729838c49bff711ae0518abd
ElliotRedhead <elliot.redhead@gmail.com>
2021-01-20 20:06:26 +0000

    Test proptypes of congrats component.

--- jotto-app/src/Congrats.jsx
+++ jotto-app/src/Congrats.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 /**
  * Functional react component for congratulatory message.
  * @param {object} props - React props.
  * @returns {JSX.Element} - Rendered component (or null ig `success` prop is false.)
  */
-export default (props) => {
+const Congrats = (props) => {
 	if (props.success) {
 		return (
 			<div data-test="component-congrats">
@@ -22,3 +23,9 @@ export default (props) => {
 		);
 	}
 };
+
+Congrats.propTypes = {
+	success: PropTypes.bool.isRequired
+};
+
+export default Congrats;

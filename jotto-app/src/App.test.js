326af970a34a53bf523c82bf3957d4e808b4c55b
ElliotRedhead <elliot.redhead@gmail.com>
2021-02-04 17:54:46 +0000

    Test if call to server is made.

--- jotto-app/src/App.test.js
+++ jotto-app/src/App.test.js
@@ -1,14 +1,22 @@
 import React from "react";
-import { shallow } from "enzyme";
+import { mount } from "enzyme";
 import { findByTestAttr } from "../test/testUtils";
 import App from "./App";
 
+import hookActions from "./actions/hookActions";
+
+const mockGetSecretWord = jest.fn();
+
 /**
  * Setup function for app component.
- * @returns {ShallowWrapper} The App component wrapper.
+ * @returns {ReactWrapper} The App component wrapper.
  */
 const setup = () => {
-	return shallow(<App />);
+	mockGetSecretWord.mockClear();
+	hookActions.getSecretWord = mockGetSecretWord;
+
+	// using mount as useeffect is not called on "shallow"
+	return mount(<App />);
 
 };
 
@@ -16,4 +24,13 @@ test("App renders without error", () => {
 	const wrapper = setup();
 	const component = findByTestAttr(wrapper, "component-app");
 	expect(component.length).toBe(1);
+});
+
+describe("getSecretWord calls", () => {
+	test("getSecretWord gets called on App mount", () => {
+		setup();
+
+		//check to see if secret word was updated
+		expect(mockGetSecretWord).toHaveBeenCalled();
+	});
 });

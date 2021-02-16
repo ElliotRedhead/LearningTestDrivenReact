8183660ad8150b383ff5f25b8104c905f1333202
ElliotRedhead <elliot.redhead@gmail.com>
2021-02-16 20:05:47 +0000

    Fulfil testing, getSecretWord on App load.

--- jotto-app/src/App.js
+++ jotto-app/src/App.js
@@ -1,6 +1,36 @@
+import React from "react";
 import "./App.css";
+import hookActions from "./actions/hookActions";
+
+/**
+ * Reducer to update state, called automatically by dispatch.
+ * @param {object} state - Existing state
+ * @param {object} action - Contains 'type' and 'payload' properties 
+ *                        e.g. { type: "setSecretWord", payload:"party" }
+ * @returns {object} - New state
+ */
+const reducer = (state, action) => {
+
+	switch(action.type) {
+	case "setSecretWord":
+		return {...state, secretWord: action.payload };
+	default:
+		throw new Error(`Invalid action type: ${action.type}`);
+	}
+	
+};
 
 const App = () => {
+
+	const [state, dispatch] = React.useReducer(reducer,{ secretWord: null});
+
+	const setSecretWord = (secretWord) => dispatch({type:"setSecretWord", payload:secretWord});
+
+	React.useEffect(
+		() => {hookActions.getSecretWord(setSecretWord);},
+		[]
+	);
+
 	return (
 		<div data-test="component-app"/>
 	);

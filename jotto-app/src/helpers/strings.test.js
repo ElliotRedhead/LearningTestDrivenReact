9535c2d083f6d406ed12e3a453e2abd83ac134b8
ElliotRedhead <elliot.redhead@gmail.com>
2021-02-18 20:20:17 +0000

    Structured testing for strings language selector.

---
+++ jotto-app/src/helpers/strings.test.js
@@ -0,0 +1,15 @@
+import stringsModule from "./strings";
+const { getStringByLanguage } = stringsModule;
+
+test("returns correct submit string for English", () => {
+
+});
+test("returns correct submit string for Emoji", () => {
+
+});
+test("returns English submit string when language does not exist", () => {
+
+});
+test("returns English submit string when submit kit does not exist for language", () => {
+	
+});

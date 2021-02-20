07fc09d7ad01b6c42a9ae4bd2752403f39fbd082
ElliotRedhead <elliot.redhead@gmail.com>
2021-02-20 21:38:21 +0000

    Structure tests for render with provider values.

--- jotto-app/src/Congrats.test.js
+++ jotto-app/src/Congrats.test.js
@@ -25,27 +25,29 @@ const setup = ({ success, language }) => {
 
 describe("languagePicker", () => {
 	test("Correctly renders congrats string in English", () => {
-
+		const wrapper = setup({ success: true });
+		expect(wrapper.text()).toBe("Congratulations! You guessed the word!");
 	});
 	test("Correctly renders congrats string in Emoji", () => {
-		
+		const wrapper = setup({ success: true, language: "emoji" });
+		expect(wrapper.text()).toBe("🎯🎉");
 	});
 });
 
 test("renders without error", () => {
-	const wrapper = setup({ success:false });
+	const wrapper = setup({ success: false });
 	const component = findByTestAttr(wrapper, "component-congrats");
 	expect(component.length).toBe(1);
 });
 
 test("renders no text when 'success' prop is false", () => {
-	const wrapper = setup({ success:false });
+	const wrapper = setup({ success: false });
 	const component = findByTestAttr(wrapper, "component-congrats");
 	expect(component.text()).toBe("");
 });
 
 test("renders non-empty congrats message when 'success' prop is true", () => {
-	const wrapper = setup({ success:true });
+	const wrapper = setup({ success: true });
 	const message = findByTestAttr(wrapper, "congrats-message");
 	expect(message.text().length).not.toBe(0);
 });

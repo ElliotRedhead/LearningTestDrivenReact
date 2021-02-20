8736279af906395a194ae4ea5fac0fcac7a83f9e
ElliotRedhead <elliot.redhead@gmail.com>
2021-02-20 18:24:54 +0000

    Testing language picker basic render & proptypes.

--- jotto-app/src/LanguagePicker.js
+++ jotto-app/src/LanguagePicker.js
@@ -1,12 +1,17 @@
 import React from "react";
+import propTypes from "prop-types";
 
 const LanguagePicker = ({ setLanguage }) => {
 
 	return(
-		<div>
+		<div data-test="component-language-picker">
 
 		</div>
 	);
+};
+
+LanguagePicker.propTypes = {
+	setLanguage: propTypes.func.isRequired
 };
 
 export default LanguagePicker;
